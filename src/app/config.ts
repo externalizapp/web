59ab3f9deae6bf86bafd2da63901398cb84fd422
jorgechp <jorgechp@yandex.com>
2022-02-20 11:08:20 +0100

    Split urls into host, port and url

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class Config {
     public static ALLOWED_LANGUAGES: Array<string> = ['ca', 'en', 'es'];
     public static DEFAULT_LANGUAGE: string = 'es';
-    public static SEARCH_URL: string = 'https://61fe97eba58a4e00173c98ef.mockapi.io/externalizapp/v1/contratos/';
-    public static VIEWER_URL: string = 'https://61fe97eba58a4e00173c98ef.mockapi.io/externalizapp/v1/contratos/{idContract}';
+    public static API_HOST: string = 'https://61fe97eba58a4e00173c98ef.mockapi.io';
+    public static API_PORT: string | number = '443';
+    public static SEARCH_URL: string = 'externalizapp/v1/contratos/';
+    public static VIEWER_URL: string = 'externalizapp/v1/contratos/{idContract}';
 }
